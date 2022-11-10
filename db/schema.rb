4be7bc2c96606cfcb7887d3f0e78caf64b16d57e
raul chagas <raul_chagas@hotmail.com>
2022-11-10 20:57:47 -0300

    Migrations done

--- db/schema.rb
+++ db/schema.rb
@@ -10,9 +10,44 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2022_11_10_230848) do
+ActiveRecord::Schema[7.0].define(version: 2022_11_10_234826) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "body_fats", force: :cascade do |t|
+    t.float "value"
+    t.bigint "user_id", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["user_id"], name: "index_body_fats_on_user_id"
+  end
+
+  create_table "goals", force: :cascade do |t|
+    t.bigint "user_id", null: false
+    t.text "description"
+    t.boolean "status"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["user_id"], name: "index_goals_on_user_id"
+  end
+
+  create_table "prescriptions", force: :cascade do |t|
+    t.string "title"
+    t.bigint "user_id", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["user_id"], name: "index_prescriptions_on_user_id"
+  end
+
+  create_table "recipes", force: :cascade do |t|
+    t.string "title"
+    t.text "description"
+    t.integer "calories"
+    t.integer "prep_time"
+    t.integer "ranking"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "users", force: :cascade do |t|
     t.string "email", default: "", null: false
@@ -26,4 +61,26 @@ ActiveRecord::Schema[7.0].define(version: 2022_11_10_230848) do
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
+  create_table "vitamins", force: :cascade do |t|
+    t.bigint "user_id", null: false
+    t.string "name"
+    t.string "dosage"
+    t.text "description"
+    t.boolean "taken"
+    t.datetime "date"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["user_id"], name: "index_vitamins_on_user_id"
+  end
+
+  create_table "weights", force: :cascade do |t|
+    t.float "value"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  add_foreign_key "body_fats", "users"
+  add_foreign_key "goals", "users"
+  add_foreign_key "prescriptions", "users"
+  add_foreign_key "vitamins", "users"
 end
