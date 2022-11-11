4fbae9fcefe449e41b6267cebfccdaf45e1e547e
raul chagas <raul_chagas@hotmail.com>
2022-11-10 21:41:59 -0300

    routes done

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,11 @@
 Rails.application.routes.draw do
   devise_for :users
   root to: "pages#home"
-  resources 
+  resources :dashboards, only: %i[index]
+  resources :goals, only: %i[index show new edit destroy]
+  resources :weights, only: %i[show new edit]
+  resources :body_fat, only: %i[show new edit]
+  resources :vitamins, only: %i[index show new edit destroy]
+  resources :recipes, only: %i[index show]
+  resources :prescriptions, only: %i[index show new edit destroy]
 end
