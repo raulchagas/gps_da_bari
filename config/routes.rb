ffeac44e9a455c50c0cc82a38a085dec05f1fc58
Marina Angeli <marinasimoes@gmail.com>
2022-11-12 12:19:02 -0300

    removed edit routes from body fat and weight

--- config/routes.rb
+++ config/routes.rb
@@ -3,8 +3,8 @@ Rails.application.routes.draw do
   root to: "pages#home"
   resources :dashboards, only: %i[index]
   resources :goals, only: %i[index show new create edit destroy]
-  resources :weights, only: %i[show new create edit]
-  resources :body_fat, only: %i[index show new create edit]
+  resources :weights, only: %i[show new create]
+  resources :body_fat, only: %i[index show new create]
   resources :vitamins, only: %i[index show new create edit destroy]
   resources :recipes, only: %i[index show]
   resources :prescriptions, only: %i[index show new create edit destroy]
