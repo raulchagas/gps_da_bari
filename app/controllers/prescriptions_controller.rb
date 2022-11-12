250d3c000810ac43fbb51951b7c8b601460b449c
Marina Angeli <marinasimoes@gmail.com>
2022-11-12 14:01:00 -0300

    created index

--- app/controllers/prescriptions_controller.rb
+++ app/controllers/prescriptions_controller.rb
@@ -1,2 +1,35 @@
 class PrescriptionsController < ApplicationController
+  def index
+    @prescriptions = Prescription.all
+  end
+
+  def new
+
+  end
+
+  def create
+
+  end
+
+  def update
+
+  end
+
+  def edit
+
+  end
+
+  def destroy
+
+  end
+
+  private
+
+  def set_prescription
+
+  end
+
+  def prescription_params
+
+  end
 end
