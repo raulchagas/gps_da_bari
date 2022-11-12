65ac804ef98f55d2484f1551674af85a2e308aa7
raul chagas <raul_chagas@hotmail.com>
2022-11-12 13:44:58 -0300

    defined controller actions

--- app/controllers/weights_controller.rb
+++ app/controllers/weights_controller.rb
@@ -1,15 +1,30 @@
 class WeightsController < ApplicationController
-
   def index
+    @weights = Weight.all
   end
 
-    def new
+  def new
+    @weight = Weight.new
   end
 
- def create
- end
+  def create
+    @weight = Weight.new(weight_params)
+    @user.weight = current_user
+    if @weight.save
+      redirect_to weights_path
+    else
+      render :new, status: :unprocessable_entity
+    end
+  end
 
- def show
- end
+  def show
+    @weight = Weight.find(params[:id])
+  end
+
+  private
+
+  def weight_params
+    params.require(:weight).permit(:user, :value)
+  end
 
 end
