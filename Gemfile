373d95b67834451f967c34c505cd00ef245ed017
Marina Angeli <marinasimoes@gmail.com>
2022-11-12 13:01:56 -0300

    installed gem faker and created seed

--- Gemfile
+++ Gemfile
@@ -28,6 +28,8 @@ gem "stimulus-rails"
 gem "jbuilder"
 
 gem 'byebug'
+
+gem 'faker'
 # Use Redis adapter to run Action Cable in production
 # gem "redis", "~> 4.0"
 
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,24 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
+puts "Cleaning database"
+Recipe.destroy_all
+
+
+puts "Creating recipes"
+
+10.times do
+  Recipe.create!(
+    title: Faker::Food.dish,
+    description: Faker::Food.description,
+    calories: rand(10..1000),
+    prep_time: rand(15, 20, 30, 40, 50, 60, 70, 80, 90, 100, 110, 120),
+    ranking: rand(0..5)
+  )
+end
+
+@recipes = Recipe.all
+
+@recipes.each do |recipe|
+  puts "#{recipe.title} - #{recipe.description} - #{recipe.calories} - #{recipe.prep_time} - #{recipe.ranking}"
+end
