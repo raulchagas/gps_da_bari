49ba51522f8eeb7a6e26eafec65d6b5bba54831f
raul chagas <raul_chagas@hotmail.com>
2022-11-20 18:11:12 -0300

    Set as done working

--- app/controllers/goals_controller.rb
+++ app/controllers/goals_controller.rb
@@ -12,14 +12,13 @@ class GoalsController < ApplicationController
 
   def create
     if params[:goal][:description].present?
-        params[:goal][:description].shift
-        params[:goal][:description].each do |description|
-        # @goal.description = description
-        # @goal.user = current_user
-        @goal = Goal.create(description: description, user: current_user)
-        end
+      params[:goal][:description].shift
+      params[:goal][:description].each do |description|
+      # @goal.description = description
+      # @goal.user = current_user
+      @goal = Goal.create(description: description, user: current_user)
+      end
     end
-
     if @goal.save
       redirect_to my_goals_path
     else
@@ -31,14 +30,25 @@ class GoalsController < ApplicationController
     @goals = Goal.where(user: current_user)
   end
 
-  def edit
-  end
+  # def edit
+  #   @goals = Goal.where(user: current_user)
+  #   @goal = Goal.find(params[:id])
+  # end
 
-  def update
-    if goal.update(goal_params)
-      redirect_to goal_path(@goal)
+  # def update
+  #   if @goal.update(status: true)
+  #     redirect_to my_goals_path
+  #   else
+  #     render :my_goals
+  #   end
+  # end
+
+  def set_as_done
+    @goal = Goal.find(params[:id])
+    if @goal.update(status: true)
+      redirect_to my_goals_path
     else
-      render :edit
+      render :my_goals
     end
   end
 
